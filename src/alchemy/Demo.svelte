25ea11e80f0d170e00b41be6c52c44a507f0bc1c
Shawn McGee <s.mcgeek07@gmail.com>
2022-10-11 00:09:43 -0600

    Working on networked physics

--- src/alchemy/Demo.svelte
+++ src/alchemy/Demo.svelte
@@ -4,26 +4,31 @@
     import * as THREE from 'three'
     import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
     import RAPIER from '@dimforge/rapier3d-compat'
-    import { TetrahedronGeometry } from "three";
     import Viewport from "./Viewport.svelte";
+
+    import * as TRYSTERO from 'trystero'
+    import Token from "./Token";
+
+    let  _is_client : boolean = false
+    let  _is_server : boolean = false
 
     let _3js_viewport : Viewport
     let _3js_camera : THREE.PerspectiveCamera
+    let _3js_orbit : OrbitControls
+
+    let _r3d_world
 
     onMount(async () => {
+        
         // setup rapier3d
-        console.log('Initializing Rapier3D...');
         await RAPIER.init();
-        const _r3d_world = new RAPIER.World({x: 0.0, y: -9.81, z: 0.0});
-        console.log('Done');
+        _r3d_world = new RAPIER.World({x: 0.0, y: -9.81, z: 0.0});
 
         // setup threejs
-        console.log('Initializing THREE.js')
         _3js_camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        const _3js_orbit_controls = new OrbitControls(_3js_camera, _3js_viewport.canvas());
-        _3js_camera.position.y =  5
-        _3js_camera.position.z = -5
-        console.log('Done');
+        _3js_orbit = new OrbitControls(_3js_camera, _3js_viewport.canvas());
+        _3js_camera.position.y =  16
+        _3js_camera.position.z = -16
 
         // create and add light in THREE
         const _3js_light = new THREE.DirectionalLight();
@@ -31,19 +36,18 @@
         _3js_viewport.add(false, _3js_light)
 
         // create and add grid helper in THREE
-        const _3js_grid_helper = new THREE.GridHelper(10, 10)
+        const _3js_grid_helper = new THREE.GridHelper(32, 32)
         _3js_viewport.add(false, _3js_grid_helper)
         _3js_grid_helper.position.y = .50125
 
         // create and add surface in THREE
-        const _3js_surface_geometry = new THREE.BoxGeometry(10, 1, 10)
-        const _3js_surface_material = new THREE.MeshBasicMaterial({color: 0x0f0f0f })
+        const _3js_surface_geometry = new THREE.BoxGeometry(32, 1, 32)
+        const _3js_surface_material = new THREE.MeshBasicMaterial({color: 0x2f2f2f })
         const _3js_surface_mesh = new THREE.Mesh(
             _3js_surface_geometry,
             _3js_surface_material
         )
-        _3js_surface_mesh.name="Surface"
-        _3js_viewport.add(true, _3js_surface_mesh)
+        _3js_viewport.add(false, _3js_surface_mesh)
 
         // create and add surface in RAPIER
         const _r3d_surface_rigidbody_desc = RAPIER.RigidBodyDesc.fixed()
@@ -51,74 +55,124 @@
             new Float32Array(_3js_surface_geometry.getAttribute('position').array),
             new Uint32Array(_3js_surface_geometry.index.array)
         ).setMassProperties(
-            1, new RAPIER.Vector3(0, 0, 0), new RAPIER.Vector3(0, 0, 0), new RAPIER.Quaternion(1, 0, 0, 0)
-        )
-        
+            1, 
+            new RAPIER.Vector3(0, 0, 0), 
+            new RAPIER.Vector3(0, 0, 0), 
+            new RAPIER.Quaternion(1, 0, 0, 0)
+        )        
         const _r3d_surface_rigidbody = _r3d_world.createRigidBody(_r3d_surface_rigidbody_desc                        )
         const _r3d_surface_collider  = _r3d_world.createCollider (_r3d_surface_collider_desc , _r3d_surface_rigidbody)
 
-        // create and add d4 in THREE
-        const _3js_d4_geometry = new THREE.TetrahedronGeometry()
-        const _3js_d4_material = new THREE.MeshBasicMaterial({color: 0xff00ff })
-        const _3js_d4_mesh = new THREE.Mesh(
-            _3js_d4_geometry,
-            _3js_d4_material
-        )
-        _3js_d4_mesh.name="D4"
-        _3js_viewport.add(true, _3js_d4_mesh)
-
-        // create and add d4 in RAPIER        
-        const _r3d_d4_rigidbody_desc = RAPIER.RigidBodyDesc.dynamic()
-        // bad juju... extracts vertex data from THREEjs in a non-typesafe manner
-        const _r3d_d4_collider_desc  = RAPIER.ColliderDesc.trimesh(
-            new Float32Array(_3js_d4_geometry.getAttribute('position').array),
-            // more bad juju
-            new Uint32Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
-        ). setMassProperties(
-            1, new RAPIER.Vector3(0, 0, 0), new RAPIER.Vector3(1, 1, 1), new RAPIER.Quaternion(1, 0, 0 ,0)
-        )
-        const _r3d_d4_rigidbody = _r3d_world.createRigidBody(_r3d_d4_rigidbody_desc                   )
-        const _r3d_d4_collider  = _r3d_world.createCollider (_r3d_d4_collider_desc , _r3d_d4_rigidbody)
-
-        _r3d_d4_rigidbody.setTranslation(new RAPIER.Vector3(0, 2, 0), true)
-        _r3d_d4_rigidbody.applyImpulseAtPoint(new RAPIER.Vector3(0, 1, 0), new RAPIER.Vector3(10, 1, 1), true)
-
-        // _r3d_d4_rigidbody.setBodyType(RAPIER.RigidBodyType.Fixed)
-
         update()
-
-        function update() {
-
-            _r3d_world.step()
-
-            const t = _r3d_d4_rigidbody.translation()
-            _3js_d4_mesh.position.x = t.x
-            _3js_d4_mesh.position.y = t.y
-            _3js_d4_mesh.position.z = t.z
-
-            const q = _r3d_d4_rigidbody.rotation()
-            _3js_d4_mesh.quaternion.x = q.x
-            _3js_d4_mesh.quaternion.y = q.y
-            _3js_d4_mesh.quaternion.z = q.z
-            _3js_d4_mesh.quaternion.w = q.w
-
-            _3js_orbit_controls.update()
-            _3js_viewport.render(_3js_camera)
-
-            requestAnimationFrame(update)
-        }
-
-        
     })
 
+    function update() {
+
+        _r3d_world.step()
+
+        Object.values(_tokens).forEach(token => {
+            token.update()
+        })
+
+        _3js_orbit.update()
+        _3js_viewport.render(_3js_camera)
+
+        requestAnimationFrame(update)
+    }
+
+    let _dragging
+
     function onMouseDown(event: MouseEvent) {
-        console.log(_3js_viewport.pick({
+        const pickable = _3js_viewport.pick({
             x: event.clientX,
             y: event.clientY
-        }, _3js_camera))
+        }, _3js_camera)
+        // JANK
+        if(pickable) Object.values(_tokens).forEach((token : Token) => {
+            if(token._3js_mesh === pickable._viewable)
+                _dragging = token
+        })
+    }
+
+    function onMouseUp(event: MouseEvent) {
+        _dragging = undefined
+    }
+
+    
+    let _tokens : object = { }
+
+    function spawn() {
+        // spawn tokens
+        for(let i = 0; i < 8; i ++)
+        for(let j = 0; j < 8; j ++) {
+            const uuid = `${i}${j}`
+            const token = new Token(uuid)
+            token._r3d_rigidbody_desc.setTranslation(
+                2 * i - 8, 1, 2 * j - 8
+            )
+            _tokens[uuid] = token
+
+            token.spawn(_3js_viewport, _r3d_world)
+        }
+    }
+
+    let _room_id : string = 'hello'
+
+    function onHost() {
+        _is_server = true
+
+        spawn()
+
+        const room = TRYSTERO.joinRoom({ appId: 'alchemyvtt' }, _room_id)
+        const [sendHandshake, getHandshake] = room.makeAction('H')
+        room.onPeerJoin((peerId) => {
+            sendHandshake({ }, peerId)
+        })
+    }
+
+    let _server_peerId : string
+
+    function onJoin() {
+        _is_client = true
+        const room = TRYSTERO.joinRoom({ appId: 'alchemyvtt' }, _room_id)
+        const [sendHandshake, getHandshake] = room.makeAction('H')
+        getHandshake((data, peerId) => {
+            if(!_server_peerId)
+                _server_peerId = peerId
+        })
     }
 
     // https://en.wikipedia.org/wiki/Platonic_solid#Cartesian_coordinates
 </script>
 
-<Viewport bind:this={_3js_viewport} on:mousedown={onMouseDown}/>
+<div class='container'>
+    <Viewport bind:this={_3js_viewport} on:mousedown={onMouseDown}/>
+
+    {#if !_is_client && !_is_server}
+    <div class='ui'>
+        <input type='button' value='Host' on:click={onHost}/>
+    
+        <input type='text'  bind:value={_room_id}/>
+        <input type='button' value='Join' on:click={onJoin}/>
+    </div>
+    {/if}
+
+</div>
+
+<style>
+    .container {
+        display:flex
+    }
+
+    .ui {
+        position:absolute;
+        top: 0px;
+        left: 0px;
+        z-index: 1;
+    }
+
+    input {
+        font-size: 32px;
+    }
+</style>
+
---
+++ src/alchemy/Token.ts
@@ -0,0 +1,73 @@
+import * as RAPIER from '@dimforge/rapier3d-compat'
+import * as THREE  from 'three'
+// import * as UUID   from 'uuid'
+
+export class Token {
+    _uuid : string
+
+    _r3d_rigidbody : RAPIER.RigidBody
+    _r3d_collider  : RAPIER.Collider
+
+    _r3d_rigidbody_desc : RAPIER.RigidBodyDesc
+    _r3d_collider_desc  : RAPIER.ColliderDesc
+
+    _3js_mesh : THREE.Mesh
+
+    constructor(uuid : string) {
+        this._uuid = uuid;
+
+        // default to cube
+        const _3js_token_geometry = new THREE.BoxGeometry(1, 1, 1)
+        const _3js_token_material = new THREE.MeshBasicMaterial({
+            color: Color.random()
+        })
+        this._3js_mesh = new THREE.Mesh(
+            _3js_token_geometry,
+            _3js_token_material
+        )
+
+        this._r3d_rigidbody_desc = RAPIER.RigidBodyDesc.fixed()
+        this._r3d_collider_desc  = RAPIER.ColliderDesc.trimesh(
+            new Float32Array(_3js_token_geometry.getAttribute('position').array),
+            new Uint32Array (_3js_token_geometry.index.array)
+        ).setMassProperties(
+            1, 
+            new RAPIER.Vector3(0, -100, 0), 
+            new RAPIER.Vector3(0,    0, 0),
+            new RAPIER.Quaternion(1, 0, 0, 0)
+        )
+    }
+
+    public spawn(_3js_viewport, _r3d_world : RAPIER.World) {
+        _3js_viewport.add(true, this._3js_mesh)
+        this._r3d_rigidbody = _r3d_world.createRigidBody(this._r3d_rigidbody_desc)
+        this._r3d_collider  = _r3d_world.createCollider (this._r3d_collider_desc )
+    }
+
+    public update() {
+        const t = this._r3d_rigidbody.translation()
+        this._3js_mesh.position.x = t.x
+        this._3js_mesh.position.y = t.y
+        this._3js_mesh.position.z = t.z
+
+        const q = this._r3d_rigidbody.rotation()
+        this._3js_mesh.quaternion.x = q.x
+        this._3js_mesh.quaternion.y = q.y
+        this._3js_mesh.quaternion.z = q.z
+        this._3js_mesh.quaternion.w = q.w
+
+        // Rapier3D does not support scale!
+    }
+}
+
+namespace Color {
+    export function random() {
+        return new THREE.Color(
+            (Math.floor((Math.random() * 127) + 128) << 16) |
+            (Math.floor((Math.random() * 127)      ) <<  8) |
+            (Math.floor((Math.random() * 127) + 128)      )
+        );
+    }
+}
+
+export default Token
